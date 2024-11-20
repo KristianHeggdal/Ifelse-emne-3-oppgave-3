ffbba13a992467fd139f7918dfede28e5556d785
KristianHeggdal <kristianheggdal@hotmail.no>
2024-11-20 19:29:28 +0100

    Add project files.

---
+++ Ifelse emne 3 oppgave 3/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Skriv inn to tall");
+var number1Input = Console.ReadLine();
+var number2Input = Console.ReadLine();
+int number1 = int .Parse(number1Input);
+int number2 = int .Parse(number2Input);
+
+bool is30 = CheckNumbers();
+CheckNumbers();
+print();
+
+bool CheckNumbers()
+{
+    if (number1 + number2 == 30 || number1 + number2 > 30)
+    {
+        return true;
+    };
+    return false;
+}
+
+void print()
+{
+    if (is30 == true)
+    {
+        Console.WriteLine("True");
+    }
+    else
+    {
+        Console.WriteLine("false");
+    }
+}
+
+;
+
